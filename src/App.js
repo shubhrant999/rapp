766c60fc3eed5883bd8cf60c2dc8999b650aa12b
Shubhrant999 <shubhrant999@gmail.com>
2023-08-23 16:15:22 +0530

    first commit rapp

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import Header from "./components/header";
+import Section from "./components/section";
+import Footer from "./components/footer";
+// ReactDOM.render(<Game />, document.getElementById("root"));
+
+
+
+function App() {
+    return (
+    <>
+        <Header />
+        <Section />
+        <Footer />
+    </>
+        
+    );
+  }
+  
+  export default App;
---
+++ src/components/section.js
@@ -0,0 +1,10 @@
+
+function Section() {
+    return (
+      <div>
+        <h1>Section !</h1>
+      </div>
+    );
+  }
+  
+  export default Section;
