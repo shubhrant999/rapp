afbf10e32678b52400ce106cf773bd84e97369f7
Shubhrant999 <shubhrant999@gmail.com>
2023-08-25 13:20:09 +0530

    b

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header() {
     <div>
       <header className='App-header'>
         <img src='' className='App-logo' alt='logo' />
-        <p>This is my header section A</p>
+        <p>This is my header section B</p>
         <a className='pp-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
